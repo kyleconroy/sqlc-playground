bf4755c19f6e1b306e3b27853719f24eb4918d2c
Kyle Conroy <kyle@conroy.org>
2020-02-27 16:42:15 -0800

    Just kidding
    Former-commit-id: 2ef144d5e3c2b960f50d7c5388f6e7437fe1f8b2

--- main.go
+++ main.go
@@ -102,10 +102,9 @@ func main() {
 		cmd := exec.CommandContext(context.Background(), "godoc", "-http=:6061")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		cmd.Env = append(os.Environ(),
-			"GOMOD=",
-			"GOPATH="+gopath,
-		)
+		cmd.Env = []string{
+			"GOPATH=" + gopath,
+		}
 		fmt.Println("Starting godoc on port :6061 with GOPATH=" + gopath)
 		if err := cmd.Run(); err != nil {
 			log.Fatal(err)
